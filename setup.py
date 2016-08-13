cd07a5767a2c1085126845c8251824a99a91f48b
Wolfgang Langner <tds333@mailbox.org>
2016-08-13 21:31:41 +0200

    Add long description

--- setup.py
+++ setup.py
@@ -10,7 +10,11 @@ setup(name='StdConfigParser',
       version=version,
       description="A standard INI style configuration parser.",
       long_description="""\
-TODO
+This is the Python configparser with an extra class StdConfigParser.
+
+Provides a standard configuration syntax and the parser for it.
+Also contains everything to be a backport of the ConfigParser from
+Python 3.5 in one module easy to vendor or install as extra dependency.
 """,
       classifiers=[
           'Development Status :: 4 - Beta',
