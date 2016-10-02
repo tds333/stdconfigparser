420be95b7c1355a7288d8f1165d1c0d0eb0d57ad
Wolfgang Langner <tds333@mailbox.org>
2016-10-02 16:57:17 +0200

    Set version to 0.8dev0

--- stdconfigparser.py
+++ stdconfigparser.py
@@ -38,7 +38,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 
-__version__ = "0.7"
+__version__ = "0.8dev0"
 
 __all__ = ["NoSectionError", "DuplicateOptionError", "DuplicateSectionError",
            "NoOptionError", "InterpolationError", "InterpolationDepthError",
