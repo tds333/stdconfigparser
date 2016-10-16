7fe127f0570f08f8781a5cd1dd0f69249e54c56e
Wolfgang Langner <tds333@mailbox.org>
2016-10-16 17:42:20 +0200

    Use spaces, no tabs

--- do.py
+++ do.py
@@ -18,41 +18,41 @@ PYTHON = VENVDIR / "py35" / "bin" / "python"
 
 
 def invalid():
-	print("Invalid action")
+    print("Invalid action")
 
 
 def docu():
-	call([str(PYTHON), "-m", "sphinx", "-b", "html", "./docu", "./build/docu/html"])
+    call([str(PYTHON), "-m", "sphinx", "-b", "html", "./docu", "./build/docu/html"])
 
 
 def test():
-	for dirname in (x for x in VENVDIR.iterdir() if x.is_dir()):
-		py = dirname / "bin" / "python"
-		call([str(py), "-m", "pytest", "test"])
+    for dirname in (x for x in VENVDIR.iterdir() if x.is_dir()):
+        py = dirname / "bin" / "python"
+        call([str(py), "-m", "pytest", "test"])
 
 
 def teststyle():
-	call([str(PYTHON), "-m", "flake8", "stdconfigparser.py"])
+    call([str(PYTHON), "-m", "flake8", "stdconfigparser.py"])
 
 
 def dist():
-	call(["python3", "setup.py", "sdist", "bdist_wheel"])
+    call(["python3", "setup.py", "sdist", "bdist_wheel"])
 
 
 commands = {
-	"test": test,
-	"docu": docu,
-	"teststyle": teststyle,
-	"dist": dist,
+    "test": test,
+    "docu": docu,
+    "teststyle": teststyle,
+    "dist": dist,
 }
 
 def main():
-	parser = argparse.ArgumentParser(description='Executor script for project.')
-	parser.add_argument('action', choices=list(commands.keys()))
-	args = parser.parse_args(sys.argv[1:])
-	action = args.action
-	commands.get(action, invalid)()
+    parser = argparse.ArgumentParser(description='Executor script for project.')
+    parser.add_argument('action', choices=list(commands.keys()))
+    args = parser.parse_args(sys.argv[1:])
+    action = args.action
+    commands.get(action, invalid)()
 
 
 if __name__ == '__main__':
-	main()
+    main()
